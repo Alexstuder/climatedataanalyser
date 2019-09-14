04f47d4e695f89dfbbefbd4509e2b8c027cea837
Alex Studer <alex@alexstuder.ch>
2019-09-14 10:11:19 -0500

    Init

---
+++ src/main/java/ch/studer/GermanClimateDataAnalyser/GermanClimateDataAnalyserApplication.java
@@ -0,0 +1,13 @@
+package ch.studer.GermanClimateDataAnalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GermanClimateDataAnalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GermanClimateDataAnalyserApplication.class, args);
+	}
+
+}
