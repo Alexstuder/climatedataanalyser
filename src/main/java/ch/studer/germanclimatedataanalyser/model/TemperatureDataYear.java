20058d905ecd6f4f00ee85d6ad27e0479bcdcb0f
Alex Studer <alex@alexstuder.ch>
2019-11-13 22:09:46 +0100

    Temperature Filled in Month Row
    - Temperature filled in Month List
    - Print Method implemented to Print the Temperature in Tab. Form

--- src/main/java/ch/studer/germanclimatedataanalyser/model/TemperatureDataYear.java
+++ src/main/java/ch/studer/germanclimatedataanalyser/model/TemperatureDataYear.java
@@ -1,9 +1,17 @@
 package ch.studer.germanclimatedataanalyser.model;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TemperatureDataYear {
+
+
+    private static final Logger log = LoggerFactory.getLogger(TemperatureDataYear.class);
 
     // All Temperature Data for Month Jan collected !
     private List<TemperatureDataMonth> jan ;
@@ -112,5 +120,124 @@ public class TemperatureDataYear {
 
     public List<TemperatureDataMonth> getDec() {
         return dec;
+    }
+
+    @Override
+    public String toString() {
+        return "TemperatureDataYear{" +
+                "jan=" + jan +
+                ", feb=" + feb +
+                ", mar=" + mar +
+                ", apr=" + apr +
+                ", mai=" + mai +
+                ", jun=" + jun +
+                ", jul=" + jul +
+                ", aug=" + aug +
+                ", sep=" + sep +
+                ", oct=" + oct +
+                ", nov=" + nov +
+                ", dec=" + dec +
+                '}';
+    }
+
+    public void printTemperatureDataYear(){
+
+        String highestYear = getHighestYear();
+        int numberRows = getNumberRows();
+
+        log.info("");
+        log.info("-------------------------------------------------------------------------------------------------------------------");
+        log.info("     |    Jan  |  Feb   |  Mar   |  Apr   |  Mai   |  Jun   |  Jul   |  Aug   |  Sep   | Oct    |  Nov   |  Dec   |");
+        log.info("-------------------------------------------------------------------------------------------------------------------");
+
+        String yearCounter = highestYear;
+        for(int i = 1 ; i <= numberRows + 2 ; i++){
+
+          log.info(getPrintLine(yearCounter));
+          yearCounter = String.valueOf(Integer.valueOf(yearCounter) - 1);
+        log.info("-------------------------------------------------------------------------------------------------------------------");
+
+        }
+
+
+
+    }
+
+    private String getPrintLine(String year) {
+        return year + " | "+getPrintMonth(jan,year) + getPrintMonth(feb,year) + getPrintMonth(mar,year)  + getPrintMonth(apr,year) + getPrintMonth(mai,year)  + getPrintMonth(jun,year) +
+               getPrintMonth(jul,year) + getPrintMonth(aug,year) + getPrintMonth(sep,year) + getPrintMonth(oct,year) + getPrintMonth(nov,year)  + getPrintMonth(dec,year)  ;
+    }
+
+    private String getPrintMonth(List<TemperatureDataMonth> months, String year) {
+        // use DecimalFormat
+        DecimalFormat decimalFormat = new DecimalFormat("#00.00##");
+        String formatedTemperature = " ------ |";
+
+        for(TemperatureDataMonth month : months){
+
+            if(month.getYear().equals(year)){
+                formatedTemperature= " " + decimalFormat.format(month.getTemperature()) + "  |";
+            }
+        }
+                return formatedTemperature;
+    }
+
+    private int getNumberRows() {
+
+        int numberRows = 0 ;
+
+        numberRows = getHighesRow(numberRows, jan);
+        numberRows = getHighesRow(numberRows, feb);
+        numberRows = getHighesRow(numberRows, mar);
+        numberRows = getHighesRow(numberRows, apr);
+        numberRows = getHighesRow(numberRows, mai);
+        numberRows = getHighesRow(numberRows, jun);
+        numberRows = getHighesRow(numberRows, jul);
+        numberRows = getHighesRow(numberRows, aug);
+        numberRows = getHighesRow(numberRows, sep);
+        numberRows = getHighesRow(numberRows, oct);
+        numberRows = getHighesRow(numberRows, nov);
+        numberRows = getHighesRow(numberRows, dec);
+
+       return numberRows;
+    }
+
+    private int getHighesRow(int numberRows, List<TemperatureDataMonth> month) {
+         if (numberRows > month.size()){
+             return numberRows;
+         } else {
+             return month.size();
+         }
+    }
+
+    public String getHighestYear(){
+        String highestYear = "0000";
+
+        highestYear = getHigher(highestYear , jan);
+        highestYear = getHigher(highestYear , feb);
+        highestYear = getHigher(highestYear , mar);
+        highestYear = getHigher(highestYear , apr);
+        highestYear = getHigher(highestYear , mai);
+        highestYear = getHigher(highestYear , jun);
+        highestYear = getHigher(highestYear , jul);
+        highestYear = getHigher(highestYear , aug);
+        highestYear = getHigher(highestYear , sep);
+        highestYear = getHigher(highestYear , oct);
+        highestYear = getHigher(highestYear , nov);
+        highestYear = getHigher(highestYear , dec);
+
+        return highestYear;
+    }
+
+    private String getHigher(String highestYear, List<TemperatureDataMonth> months) {
+        String higher = highestYear;
+
+        for(TemperatureDataMonth month : months){
+            if(Integer.valueOf(higher) < Integer.valueOf(month.getYear())){
+                higher = month.getYear();
+            }
+        }
+
+        return higher;
     }
 }
