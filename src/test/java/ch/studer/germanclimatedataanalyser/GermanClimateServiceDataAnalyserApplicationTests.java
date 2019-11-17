9fb315282e82b396ae11faf7b43811776ede414f
Alex Studer <alex@alexstuder.ch>
2019-11-17 20:51:18 +0100

    Deactivate IntegrationTest

--- src/test/java/ch/studer/germanclimatedataanalyser/GermanClimateServiceDataAnalyserApplicationTests.java
+++ src/test/java/ch/studer/germanclimatedataanalyser/GermanClimateServiceDataAnalyserApplicationTests.java
@@ -5,11 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class GermanClimateServiceDataAnalyserApplicationTests {
 
-	@Test
+	// TODO Activate IntegrationTest !
+	//@Test
 	public void contextLoads() {
 	}
 
