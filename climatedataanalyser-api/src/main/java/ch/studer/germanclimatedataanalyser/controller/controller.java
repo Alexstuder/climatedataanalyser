633e1235f65649061d9d314e536d2b24fd23d4d0
Alex Studer <alex@alexstuder.ch>
2020-01-15 22:52:44 +0100

    fooling around with some random logging properties to get an log on Tomcat !!??

--- climatedataanalyser-api/src/main/java/ch/studer/germanclimatedataanalyser/controller/controller.java
+++ climatedataanalyser-api/src/main/java/ch/studer/germanclimatedataanalyser/controller/controller.java
@@ -1,12 +1,34 @@
 package ch.studer.germanclimatedataanalyser.controller;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequestMapping("/api/status")
+@CrossOrigin
 public class controller {
+
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(controller.class);
+
+
+    @GetMapping("/")
+    public List<String> handle() throws Exception{
+        List<String> status = new ArrayList<String>();
+
+        LOG.debug("Server is Running !");
+        status.add("Server is running !");
+        return status;
+
+    }
 //
 //        @Autowired
 //        JobLauncher jobLauncher;
