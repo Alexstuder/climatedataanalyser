192bcc4ca2c18c6d8809786fec3d7666de166240
Alex Studer <alex@alexstuder.ch>
2020-03-05 17:30:43 +0100

    ClimateHistory Method:stationId has to exist in every Period --transfer

--- climatedataanalyser-api/src/main/java/ch/studer/germanclimatedataanalyser/service/ClimateHistoryAnalyserServiceImpl.java
+++ climatedataanalyser-api/src/main/java/ch/studer/germanclimatedataanalyser/service/ClimateHistoryAnalyserServiceImpl.java
@@ -56,6 +56,83 @@ public class ClimateHistoryAnalyserServiceImpl implements ClimateHistoryAnalyser
 
     @Override
     public List<ClimateHistoryDto> getClimateHistoryEveryStationExistsData(String originYear, List<StationClimate> stationClimates) {
-        return null;
+
+        // Return a Result List
+        List<ClimateHistoryDto> climateHistoryDtos = new ArrayList<ClimateHistoryDto>();
+
+        //List with all Temperature ;needed to collect and calculate the average temperature
+        List<TemperatureForMonths> tmpTemperatureForMonths;
+
+        // **************************************************************
+        // Eine Liste aller StationsId aus der stationClimates anfertigen, welche durch alle Perioden exisiteren
+        // **************************************************************
+
+        List<Integer> stationList = new ArrayList<Integer>();
+
+        int noResult = 0 ;
+        boolean notFound ;
+        boolean firstRun = true ;
+        String endPeriod = originYear;
+
+        // Just to make sure that we didnt hit a hole ! Do it 5 times !
+        // anyway 6 x 30 endPeriod = 1840 ; before this date we should not care too much about climate history
+        while(noResult <= 5){
+
+            List<Integer> tmpStationList = new ArrayList<Integer>();
+            tmpTemperatureForMonths = new ArrayList<TemperatureForMonths>();
+            ClimateHistoryDto climateHistoryDto = new ClimateHistoryDto();
+            notFound = true;
+
+            for (StationClimate stationClimate : stationClimates){
+
+                if (stationClimate.getEndPeriod().contentEquals(endPeriod)){
+                    tmpStationList.add(stationClimate.getStationId());
+                    notFound = false;
+                }
+            }
+            //if First Run get all tmStationList into stationList
+            if(firstRun){
+                stationList = tmpStationList;
+                firstRun = false;
+            } else {
+                stationList = checkIfExistsInCollection(tmpStationList,stationList);
+            }
+
+            //check if there was noResult in this run !
+            if (notFound) {
+                noResult++;
+            }
+
+
+            //calculate the next period
+            endPeriod = String.valueOf(Integer.parseInt(endPeriod) - 30);
+        }
+
+        climateHistoryDtos = getClimateHistoryDtosIfExistsInStationList(climateHistoryDtos,stationList);
+
+
+
+        // Alle Records aus StationClimates sammeln, welche auch in der Liste der oben angefertigten StationId List existieren
+
+
+
+
+        // Liste aller climateHistoryDtos zurückgeben
+        return climateHistoryDtos;
+    }
+
+
+    private List<Integer> checkIfExistsInCollection(List<Integer> tmpStationList, List<Integer> stationList) {
+        List<Integer> newStationList = new ArrayList<Integer>();
+
+        // Check if tmpStationId exists in collection of existing stationIds
+        for(Integer tmpStationId : tmpStationList){
+            if(stationList.indexOf(tmpStationId) != -1){
+                newStationList.add(tmpStationId);
+            }
+        }
+
+
+        return newStationList;
     }
 }
