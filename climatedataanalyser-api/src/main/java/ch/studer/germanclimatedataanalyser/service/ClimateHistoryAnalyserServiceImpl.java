9805bf857eda8adc168c092f2181370f986ebff7
Alex Studer <alex@alexstuder.ch>
2020-03-05 19:42:31 +0100

    ClimateHistory Method implemented!

--- climatedataanalyser-api/src/main/java/ch/studer/germanclimatedataanalyser/service/ClimateHistoryAnalyserServiceImpl.java
+++ climatedataanalyser-api/src/main/java/ch/studer/germanclimatedataanalyser/service/ClimateHistoryAnalyserServiceImpl.java
@@ -3,12 +3,15 @@ package ch.studer.germanclimatedataanalyser.service;
 import ch.studer.germanclimatedataanalyser.model.database.StationClimate;
 import ch.studer.germanclimatedataanalyser.model.database.TemperatureForMonths;
 import ch.studer.germanclimatedataanalyser.model.dto.ClimateHistoryDto;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ClimateHistoryAnalyserServiceImpl implements ClimateHistoryAnalyserService {
 
+   // TODO : yearDifference as Parameter from GUI
+    int yearDifference = 30;
     @Override
     public List<ClimateHistoryDto> getClimateHistoryAverageData(String originYear, List<StationClimate> stationClimates) {
         //Build the ClimateHistoryDtos
@@ -48,7 +51,7 @@ public class ClimateHistoryAnalyserServiceImpl implements ClimateHistoryAnalyser
             }
 
             //calculate the next period
-            endPeriod = String.valueOf(Integer.parseInt(endPeriod) - 30);
+            endPeriod = String.valueOf(Integer.parseInt(endPeriod) - yearDifference);
 
         }
         return climateHistoryDtos;
@@ -57,32 +60,41 @@ public class ClimateHistoryAnalyserServiceImpl implements ClimateHistoryAnalyser
     @Override
     public List<ClimateHistoryDto> getClimateHistoryEveryStationExistsData(String originYear, List<StationClimate> stationClimates) {
 
-        // Return a Result List
-        List<ClimateHistoryDto> climateHistoryDtos = new ArrayList<ClimateHistoryDto>();
 
-        //List with all Temperature ;needed to collect and calculate the average temperature
-        List<TemperatureForMonths> tmpTemperatureForMonths;
+        // **************************************************************
+        // Eine Liste aller StationsId aus der stationClimates anfertigen, welche durch alle Perioden exisiteren
+        // **************************************************************
+        List<Integer> stationList = getStationIdExistsInEveryPeriod(originYear,stationClimates);
+
+        // **************************************************************
+        // Alle Records aus StationClimates sammeln, welche auch in der Liste der oben angefertigten StationId List existieren
+        // **************************************************************
+
+        List<ClimateHistoryDto> climateHistoryDtos = getClimateHistoryEveryStationExistsInStationList(originYear,stationClimates,stationList);
+
+
+
+        // Liste aller climateHistoryDtos zurückgeben
+        return climateHistoryDtos;
+    }
+
+    private List<Integer> getStationIdExistsInEveryPeriod(String originYear, List<StationClimate> stationClimates) {
 
         // **************************************************************
         // Eine Liste aller StationsId aus der stationClimates anfertigen, welche durch alle Perioden exisiteren
         // **************************************************************
 
         List<Integer> stationList = new ArrayList<Integer>();
-
-        int noResult = 0 ;
-        boolean notFound ;
-        boolean firstRun = true ;
+        int resultCounter = 0 ;
         String endPeriod = originYear;
+        boolean firstRun = true ;
+        boolean notFound ;
 
         // Just to make sure that we didnt hit a hole ! Do it 5 times !
         // anyway 6 x 30 endPeriod = 1840 ; before this date we should not care too much about climate history
-        while(noResult <= 5){
-
+        while(resultCounter <= 5){
             List<Integer> tmpStationList = new ArrayList<Integer>();
-            tmpTemperatureForMonths = new ArrayList<TemperatureForMonths>();
-            ClimateHistoryDto climateHistoryDto = new ClimateHistoryDto();
             notFound = true;
-
             for (StationClimate stationClimate : stationClimates){
 
                 if (stationClimate.getEndPeriod().contentEquals(endPeriod)){
@@ -91,36 +103,73 @@ public class ClimateHistoryAnalyserServiceImpl implements ClimateHistoryAnalyser
                 }
             }
             //if First Run get all tmStationList into stationList
-            if(firstRun){
-                stationList = tmpStationList;
-                firstRun = false;
-            } else {
-                stationList = checkIfExistsInCollection(tmpStationList,stationList);
+            if(!notFound){
+                if(firstRun){
+                    stationList = tmpStationList;
+                    firstRun = false;
+                } else {stationList = checkIfExistsInCollection(tmpStationList,stationList);
+                }
             }
 
-            //check if there was noResult in this run !
-            if (notFound) {
-                noResult++;
-            }
+            //check if there was resultCounter in this run !
+            if (notFound) {resultCounter++;}
 
 
             //calculate the next period
-            endPeriod = String.valueOf(Integer.parseInt(endPeriod) - 30);
+            endPeriod = String.valueOf(Integer.parseInt(endPeriod) - yearDifference);
         }
 
-        climateHistoryDtos = getClimateHistoryDtosIfExistsInStationList(climateHistoryDtos,stationList);
-
-
-
-        // Alle Records aus StationClimates sammeln, welche auch in der Liste der oben angefertigten StationId List existieren
-
-
-
-
-        // Liste aller climateHistoryDtos zurückgeben
-        return climateHistoryDtos;
+     return  stationList;
     }
 
+
+    private List<ClimateHistoryDto> getClimateHistoryEveryStationExistsInStationList(String originYear, List<StationClimate> stationClimates, List<Integer> stationList) {
+        List<ClimateHistoryDto> climateHistoryDtos = new ArrayList<ClimateHistoryDto>();
+
+        //Just for collecting  all Records and calculate the average temperature
+        List<TemperatureForMonths> tmpTemperatureForMonths ;
+
+        int noResult = 0 ;
+        String endPeriod = originYear;
+
+        // Just to make sure that we didnt hit just a hole ! Do it 5 times !
+        // anyway 6 x 30 endPeriod = 1840 ; before this date we should not care too much about climate history
+        while(noResult <= 5){
+
+            tmpTemperatureForMonths = new ArrayList<TemperatureForMonths>();
+            ClimateHistoryDto climateHistoryDto = new ClimateHistoryDto();
+            for (StationClimate stationClimate : stationClimates){
+
+                if (stationClimate.getEndPeriod().contentEquals(endPeriod)){
+
+                    if(stationList.indexOf(stationClimate.getStationId()) > -1) {
+                        if (climateHistoryDto.getEndPeriod().isEmpty()){
+                            climateHistoryDto.setEndPeriod(stationClimate.getEndPeriod());
+                            climateHistoryDto.setStartPeriod(stationClimate.getStartPeriod());
+                        }
+                        tmpTemperatureForMonths.add(stationClimate.getTemperatureForMonths());
+
+                    }
+                }
+            }
+            if (tmpTemperatureForMonths.size()>0){
+                climateHistoryDto.setClimatesMapFrom(new TemperatureForMonths().getAverage(tmpTemperatureForMonths));
+                climateHistoryDtos.add(climateHistoryDto);
+            }
+
+
+            //check if there was noResult in this run !
+            if (tmpTemperatureForMonths.size() == 0 ) {
+                noResult++;
+            }
+
+            //calculate the next period
+            endPeriod = String.valueOf(Integer.parseInt(endPeriod) - yearDifference);
+
+        }
+
+        return  climateHistoryDtos;
+    }
 
     private List<Integer> checkIfExistsInCollection(List<Integer> tmpStationList, List<Integer> stationList) {
         List<Integer> newStationList = new ArrayList<Integer>();
