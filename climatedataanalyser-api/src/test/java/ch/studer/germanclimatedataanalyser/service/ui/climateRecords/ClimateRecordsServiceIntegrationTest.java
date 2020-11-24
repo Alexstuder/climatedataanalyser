10154b580814056fe293eb165b93afcdbc578b4b
Alex Studer at Mac Book Pro <alex@alexstuder.ch>
2020-11-24 18:08:06 +0100

    transfer
    Done ! Implement getDifference
    
    next : Heureka BackBackend is done : Now : Implement a decent IntegrationTest for ClimateRecordServiceImpl  !

--- climatedataanalyser-api/src/test/java/ch/studer/germanclimatedataanalyser/service/ui/climateRecords/ClimateRecordsServiceIntegrationTest.java
+++ climatedataanalyser-api/src/test/java/ch/studer/germanclimatedataanalyser/service/ui/climateRecords/ClimateRecordsServiceIntegrationTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.jdbc.Sql;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:test-it.properties")
-@Sql({"classpath:StationClimate/H2Schema.sql","classpath:ClimateRecordService/climates.sql", "classpath:StationClimate/stations.sql"})
+@Sql({"classpath:ClimateRecordService/H2Schema.sql","classpath:ClimateRecordService/climates.sql", "classpath:ClimateRecordService/stations.sql"})
 //@Sql({"classpath:ClimateRecordService/H2Schema.sql", "classpath:ClimateRecordService/climates.sql", "classpath:ClimateRecordService/stations.sql"})
 
 public class ClimateRecordsServiceIntegrationTest {
@@ -26,14 +26,15 @@ public class ClimateRecordsServiceIntegrationTest {
     void testClimateRecordServiceWithBundesland() {
 
         final String BUNDESLAND = "Baden-Württemberg";
-        final String GPS_LAT_1 = "";
-        final String GPS_LONG_1 = "";
-        final String GPS_LAT_2 = "";
-        final String GPS_LONG_2 = "";
-        final String YEAR_FROM = "1961";
-        final String YEAR_DISTANCE = "1961";
+        final String GPS_LAT_1 = "0";
+        final String GPS_LONG_1 = "0";
+        final String GPS_LAT_2 = "0";
+        final String GPS_LONG_2 = "0";
+        final String YEAR_FROM = "1960";
+        final String YEAR_DISTANCE = "1";
 
         ClimateRecordsDto climateRecordsDto = climateRecordService.getClimateRecords(BUNDESLAND, GPS_LAT_1, GPS_LONG_1, GPS_LAT_2, GPS_LONG_2, YEAR_FROM, YEAR_DISTANCE);
+       // climateRecordsDto.getClimateRecordList().get(99999);
 
     }
 
@@ -45,8 +46,8 @@ public class ClimateRecordsServiceIntegrationTest {
         final String GPS_LONG_1 = "10";
         final String GPS_LAT_2 = "10";
         final String GPS_LONG_2 = "20";
-        final String YEAR_FROM = "1961";
-        final String YEAR_DISTANCE = "1961";
+        final String YEAR_FROM = "1960";
+        final String YEAR_DISTANCE = "5";
 
         ClimateRecordsDto climateRecordsDto = climateRecordService.getClimateRecords(BUNDESLAND, GPS_LAT_1, GPS_LONG_1, GPS_LAT_2, GPS_LONG_2, YEAR_FROM, YEAR_DISTANCE);
 
