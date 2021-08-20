43fb8ffbead9d15db5a36927db191d103044ba8f
Alex Studer at Mac Book Pro <alex@alexstuder.ch>
2021-08-20 18:27:21 +0200

    more context printout in tomcat

--- climatedataanalyser-api/src/main/java/ch/studer/germanclimatedataanalyser/batch/tasklet/DirectoryUtilityImpl.java
+++ climatedataanalyser-api/src/main/java/ch/studer/germanclimatedataanalyser/batch/tasklet/DirectoryUtilityImpl.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -26,9 +25,11 @@ public class DirectoryUtilityImpl implements DirectoryUtility {
 
     private ApplicationContext applicationContext;
     private ServletContext servletContext = null;
-    static private String tomcatRootPath=null;
+    static private String tomcatRootPath = null;
     static private String path;
-
+    static private String contextPath;
+    private static final String WEBAPPS = "/webapps";
+    private static final String DATAFILES ="/dataFiles/";
 
     private static final Logger log = LoggerFactory.getLogger(DirectoryUtilityImpl.class);
 
@@ -37,9 +38,12 @@ public class DirectoryUtilityImpl implements DirectoryUtility {
         this.applicationContext = appCont;
         this.servletContext = context;
         // If it running in a Tomcat Server this method will be called
-        tomcatRootPath =  System.getProperty( "catalina.base" );
+        tomcatRootPath = System.getProperty("catalina.base");
+        contextPath = context.getContextPath();
         log.info("context.getcontextpath:" + context.getContextPath());
-        log.info("catalina.base :" + System.getProperty( "catalina.base" ));
+        log.info("context.getMajorVersion:" + context.getMajorVersion());
+        log.info("context.getMinorVersion:" + context.getMinorVersion());
+        log.info("catalina.base :" + System.getProperty("catalina.base"));
     }
 
     private static void deleteDirectoryFiles(File directory) {
@@ -62,44 +66,39 @@ public class DirectoryUtilityImpl implements DirectoryUtility {
     static File createDir(String directoryName) {
 
         //not running in tomcat = running with junit
-        if(tomcatRootPath == null) {
-            path=System.getProperty("user.dir");
+        if (tomcatRootPath == null) {
+            path = System.getProperty("user.dir");
         } else {
-            path=tomcatRootPath+"/webapps/ClimateAnalyser";
+            //for Tomcat build path like : /opt/tomcat/webapps/ClimateAnalyser/dataFiles/
+            path = tomcatRootPath + WEBAPPS + contextPath + DATAFILES;
+            log.info("Path to files :" + path);
         }
 
         File directory = null;
         try {
-           // path = Paths.get(rootPath.getFile().getPath() + "/" + directoryName);
-
-
-            log.info("catalina.base :" + System.getProperty( "catalina.base" ));
+            log.info("catalina.base :" + System.getProperty("catalina.base"));
             log.info("User.dir :" + System.getProperty("user.dir"));
             log.info("Path.absolut Paths :" + Paths.get("").toAbsolutePath().toString());
             log.info("File.absolutFile :" + new File("").getAbsoluteFile());
 
-//            log.info("getClass.getClassLoader :" + DirectoryUtilityImpl.class.getClass().getClassLoader().getResource("").getPath() );
             log.info("Path.of.toAbsolutPath :" + Path.of("").toAbsolutePath().toString());
             log.info("FileSystem.getDefault :" + FileSystems.getDefault().getPath(".").toAbsolutePath());
             log.info("FileSystem.getDefault :" + FileSystems.getDefault().getPath("WEB-INF"));
 
-            directory = new File(path + "/dataFiles/" + directoryName);
+            directory = new File(path  + directoryName);
             Files.deleteIfExists(directory.toPath());
             deleteDirectoryFiles(directory);
             Files.createDirectories(directory.toPath());
-            log.info("Directory created:" + directory.getAbsolutePath() );
+            log.info("Directory created:" + directory.getAbsolutePath());
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
         return directory;
     }
 
-    public static File getDirectory(String folderName){
+    public static File getDirectory(String folderName) {
 
-        return new File("dataFiles/" + folderName);
-        //return Paths.get(rootPath.getFile().getPath() + "/" + folderName).toFile();
+        return new File(path + folderName);
     }
 
 }
