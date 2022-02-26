beccecaf961b6df72bad6ac87a04b1c3f177496a
Alex Studer at Mac Book Pro <alex@alexstuder.ch>
2022-02-26 16:20:49 +0100

    remove deprecated message from angular

--- climatedataanalyser-ng/src/app/database/database.component.ts
+++ climatedataanalyser-ng/src/app/database/database.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../shared/api.service';
 import {HttpEventType} from '@angular/common/http';
 import {DbLoadResponseDto} from './model/DbLoadResponseDto';
@@ -13,40 +13,56 @@ export class DatabaseComponent implements OnInit {
   dbLoadResponseDto: DbLoadResponseDto;
   columns: string[];
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {
+  }
 
   ngOnInit() {
 
     this.columns = this.apiService.getColumns();
-    this.apiService.initDbLoad().subscribe(
 
-    value => {
+    /*    this.apiService.initDbLoad().subscribe(
+          value => {
 
-      switch (value.type) {
-        case HttpEventType.Response:
-          this.dbLoadResponseDto = value.body;
+            switch (value.type) {
+              case HttpEventType.Response:
+                this.dbLoadResponseDto = value.body;
 
+            }
+          },
+          error => {
+            alert('An error occurred ,while getting DB informations from DB-Server');
+          }
+        );*/
+
+
+    // ----- new
+    this.apiService.initDbLoad().subscribe({
+        complete: () => {
+        }, // completeHandler
+        error: () => {
+        },    // errorHandler
+        next: (value) => {
+          switch (value.type) {
+            case HttpEventType.Response:
+              this.dbLoadResponseDto = value.body;
+          }
+        },
       }
-    },
-    error => {
-      alert('An error occurred ,while getting DB informations from DB-Server');
-    }
-  );
+    );
   }
 
   loadDataBase() {
     if (confirm('Do you really want to load the Database ? (Takes about 20 min)')) {
-     this.apiService.loadDataBase().subscribe(
-       value => {
+      this.apiService.loadDataBase().subscribe(
+        value => {
 
-         alert('DataBase Successfully loaded !');
+          alert('DataBase Successfully loaded !');
 
-       },
-       error => {
-         alert(error.toString());
-       }
-
-     );
+        },
+        error => {
+          alert(error.toString());
+        }
+      );
 
 
     }
