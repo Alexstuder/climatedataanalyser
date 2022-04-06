6f9a247aa475bce4c72a4c1eaf12d13b7805562c
Alex Studer <alex@alexstuder.ch>
2022-04-06 21:58:23 +0200

    spring boot ugrade

--- climatedataanalyser-api/src/main/java/ch/studer/germanclimatedataanalyser/service/ui/dbController/DbLoadInformationServiceImpl.java
+++ climatedataanalyser-api/src/main/java/ch/studer/germanclimatedataanalyser/service/ui/dbController/DbLoadInformationServiceImpl.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
+
 public class DbLoadInformationServiceImpl implements DbLoadInformationService {
 
     @SuppressWarnings("SpringJavaAutowiredMembersInspection")
